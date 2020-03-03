196a46d4e0c676c3d5a7e4357034f51557fb7222
github username <github email address>
2020-03-03 16:11:02 -0500

    fixed on user app : changed routes for user show, edit. Add validation on users edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+
+  # def current_user
+  #   User.find(session[:user_id])
+  # end
+
+  # def logged_in?
+  #   !!current_user
+  # end
+
+  # def authorized
+  #   redirect_to path
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   resources :posts
   resources :user_locations
   resources :locations, only:[:index, :show, :new, :create]
-  resources :users
+  resources :users, only:[:index, :new, :create, :update, :destroy]
   get '/sessions/log_in', to: 'sessions#authentication', as: 'loggin'
   get '/sessions/log_out', to: 'sessions#log_out', as: 'log_out'
+  get '/users/profile', to: 'users#show'
+  get '/users/profile/edit', to: 'users#edit', as: 'user_edit'
 
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
